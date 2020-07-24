a99f2c3998cf00a27c34db208cda14505797832c
EvanCharles16 <evancharles1609@gmail.com>
2020-07-24 16:21:52 +0700

    add model id

--- src/app/mock-event.ts
+++ src/app/mock-event.ts
@@ -2,6 +2,7 @@ import { Event } from './event';
 
 export const DataEvent: Event[] = [
   {
+    id: 1,
     title: 'Get Your VIRTUAL Teach On',
     imageEvent:
       'https://api.indrakawasan.com/public/images/2020-05-21T17:29:01.231Zevent1.jpeg',
@@ -11,6 +12,7 @@ export const DataEvent: Event[] = [
     location: 'Online',
   },
   {
+    id: 2,
     title: 'ViDa 2020',
     imageEvent:
       'https://api.indrakawasan.com/public/images/2020-05-21T17:37:43.615Zvida.jpeg',
@@ -20,6 +22,7 @@ export const DataEvent: Event[] = [
     location: 'Online',
   },
   {
+    id: 3,
     title: 'Shawn Mendes The Virtual Tour',
     imageEvent:
       'https://api.indrakawasan.com/public/images/2020-05-21T18:01:46.755Z6.png',
@@ -29,6 +32,7 @@ export const DataEvent: Event[] = [
     location: 'Online',
   },
   {
+    id: 4,
     title: 'The Virtual Beer Festival',
     imageEvent:
       'https://api.indrakawasan.com/public/images/2020-05-21T18:10:44.371Z9.jpeg',
